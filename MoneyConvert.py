47854729932a3258634d004962332982a6b25634
charjiez <37368693+charjiez@users.noreply.github.com>
2018-03-21 16:17:59 +0800

    Delete MoneyConvert.py

--- MoneyConvert.py
+++
@@ -1,9 +0,0 @@
-#MoneyConvert.py
-MoneyStr = input()
-if MoneyStr[0:3] in ["RMB"]:
-    USD = eval(MoneyStr[3:]) / 6.78
-    print("USD{:.2f}".format(USD))
-else:
-    RMB = eval(MoneyStr[3:]) * 6.78
-    print("RMB{:.2f}".format(RMB))
-
